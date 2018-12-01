2f7d790fb4d6dd287b0483e6e91d329c73054bb6
Samrith Shankar <samrith.s@media.net>
2018-12-01 17:07:56 +0530

    - Moved `completed` and `running` counts into `tasks`. - Added `start` method. - Added `autoStart` config key. - Moved `concurrency` into config. - Changed `working` to `__working`. - Calling `startCheckAndRun` over `startCheck` and `run` individually. - Used utils to check for functions, numerals and arrays.

--- src/index.js
+++ src/index.js
@@ -1,41 +1,67 @@
-import { startCheck, run } from './PrivateFunctions';
+import { startCheckAndRun } from './PrivateFunctions';
+import log from './log';
+
+import { assignFunction, isFunction, isArray, isNumber } from './util';
 
 export default class ConcurrentTasks {
-    constructor(concurrency) {
-        this.concurrency = typeof concurrency === 'number' ? concurrency : 3;
+    constructor(config = {}) {
+        const { concurrency, onStart, onDone, onEnd, ...otherConfig } = config;
+        this.config = {
+            autoStart: true,
+            ...otherConfig
+        };
+        this.concurrency = isNumber(concurrency) ? concurrency : 3;
+        this.onStart = assignFunction(onStart);
+        this.onDone = assignFunction(onDone);
+        this.onEnd = assignFunction(onEnd);
     }
-    working = false;
-    completed = 0;
-    running = 0;
-    onEnd;
-    onStart;
+    __working = false;
     tasks = {
         list: [],
-        total: 0
+        total: 0,
+        completed: 0,
+        running: 0
     };
     duration = {
         start: 0,
         end: 0,
-        total: 0,
-        idle: 0
+        total: 0
+    };
+
+    start = () => {
+        if (this.__working) {
+            log('already_running');
+            return false;
+        }
+
+        if (this.config.autoStart) {
+            log('auto_start_true');
+            return false;
+        }
+
+        startCheckAndRun.call(this);
+        return true;
     };
 
     add = task => {
+        const { autoStart } = this.config;
         this.tasks.list.push(task);
         this.tasks.total++;
-        startCheck.call(this);
-        run.call(this);
+        if (autoStart) {
+            startCheckAndRun.call(this);
+        }
     };
 
     addMultiple = tasks => {
-        if (
-            tasks.constructor === Array &&
-            tasks.every(t => typeof t === 'function')
-        ) {
-            this.tasks.list = tasks;
-            this.tasks.total = tasks.length;
-            startCheck.call(this);
-            run.call(this);
+        if (isArray(tasks) && tasks.every(t => isFunction(t))) {
+            const { autoStart } = this.config;
+            this.tasks = {
+                list: tasks,
+                total: tasks.length
+            };
+            if (autoStart) {
+                startCheckAndRun.call(this);
+            }
         }
     };
 
