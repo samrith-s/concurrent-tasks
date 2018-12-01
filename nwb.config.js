d1af0e87dd15083948a8306b7721790943d6a3d1
Samrith Shankar <samrith.s@media.net>
2018-12-01 18:30:58 +0530

    Changed global to TaskRunner from ConcurrentTasks

--- nwb.config.js
+++ nwb.config.js
@@ -3,7 +3,7 @@ module.exports = {
     npm: {
         esModules: true,
         umd: {
-            global: 'ConcurrentTasks',
+            global: 'TaskRunner',
             externals: {}
         }
     }
