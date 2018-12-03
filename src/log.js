785b8850ce27d23f6849bd2899316e058d54f634
Samrith Shankar <samrith.s@media.net>
2018-12-03 12:30:23 +0530

    Made log return string and handled logging in respective files

--- src/log.js
+++ src/log.js
@@ -1,7 +1,12 @@
 const logs = {
-    already_running: 'Cannot start ConcurrentTasks as it is already running!',
+    already_running:
+        'Cannot start TaskRunner instance as it is already running!',
     auto_start_true:
-        'Cannot programmatically start ConcurrentTasks as autoStart is true!'
+        'Cannot programmatically start TaskRunner instance as autoStart is true!',
+    add_requires_function:
+        'The "add" operation requires a function. Check if the first argument is a function!',
+    add_multiple_requires_array_of_functions:
+        'The "addMultiple" operation requires an array of functions. Check if the first argument is an array and whether all the items in the array are functions!'
 };
 
-export default logKey => console.warn(`ConcurrentTasks: ${logs[logKey]}`);
+export default logKey => `TaskRunner: ${logs[logKey]}`;
