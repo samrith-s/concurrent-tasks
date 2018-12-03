2832ef825c6b4b40eb12173ce8f770326bc4055c
Samrith Shankar <samrith.s@media.net>
2018-12-03 18:52:25 +0530

    Added programmatic start task runner

---
+++ demo/src/examples/programmatic-start-taskrunner.js
@@ -0,0 +1,50 @@
+import TaskRunner from '../../../es';
+import { getElements } from './util/helper';
+import { multipleTasks } from './util/task';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const {
+        element,
+        console: { start, done, end },
+        concurrency,
+        button
+    } = getElements('example-2');
+    const startButton = element.querySelector('.start');
+
+    const runner = new TaskRunner({
+        concurrency: concurrency.value,
+        autoStart: false,
+        onStart({ duration: { start: startDate } }) {
+            console.log('called start');
+            start.innerText = `Added 1000 tasks on ${new Date(startDate)}`;
+        },
+        onDone({ completed, total }) {
+            done.innerText = `✅ Completed ${completed} of ${total} tasks with ${
+                runner.concurrency
+            } concurrency`;
+        },
+        onEnd({ duration: { total } }) {
+            button.removeAttribute('disabled');
+            end.innerText = `✨ Completed all tasks in: ${(
+                total / 1000
+            ).toFixed(2)}s`;
+        }
+    });
+
+    button.onclick = () => {
+        button.setAttribute('disabled', true);
+        start.innerText = 'Added 1000 tasks!';
+        done.innerText = '';
+        end.innerText = '';
+
+        multipleTasks(runner, 1000);
+        console.log(runner);
+    };
+
+    startButton.onclick = () => {
+        button.setAttribute('disabled', true);
+        startButton.setAttribute('disabled', true);
+        runner.setConcurrency(parseInt(concurrency.value, 10));
+        runner.start();
+    };
+});
