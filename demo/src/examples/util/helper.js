ce4a1bf9e01a5a47c6c320f5bffa348192d82cb0
Samrith Shankar <samrith.s@media.net>
2018-12-04 04:02:18 +0530

    Helper gets more elements

--- demo/src/examples/util/helper.js
+++ demo/src/examples/util/helper.js
@@ -1,18 +1,27 @@
 export const getElements = id => {
     const element = document.getElementById(id);
+    const progress = element.querySelector('.progress-inner');
     const button = element.querySelector('.btn');
     const concurrency = element.querySelector('.concurrency');
+    const input = concurrency.querySelector('input');
+    const set = concurrency.querySelector('.set-concurrency');
     const console = element.querySelector('.console');
+    const info = console.querySelector('.console__info');
     const start = console.querySelector('.console__on-start');
     const done = console.querySelector('.console__on-done');
     const end = console.querySelector('.console__on-end');
 
     return {
         element,
-        concurrency,
+        progress,
+        concurrency: {
+            input,
+            set
+        },
         button,
         console: {
             console,
+            info,
             start,
             done,
             end
