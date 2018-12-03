16cf581d80b0d15c31f32a6f7300b1e1297bbee9
Samrith Shankar <samrith.s@media.net>
2018-12-03 17:36:16 +0530

    Updated basic taskrunner to set concurrency properly

--- demo/src/examples/basic-taskrunner.js
+++ demo/src/examples/basic-taskrunner.js
@@ -3,34 +3,37 @@ import { getElements } from './util/helper';
 import { queueTasks } from './util/task';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const runner = new TaskRunner({
-        concurrency: 100
-    });
-
     const {
         console: { start, done, end },
+        concurrency,
         button
     } = getElements('example-1');
 
+    const runner = new TaskRunner({
+        concurrency: concurrency.value,
+        onStart({ duration: { start: startDate } }) {
+            start.innerText = `Added 1000 tasks on ${new Date(startDate)}`;
+        },
+        onDone({ completed, total }) {
+            done.innerText = `✅ Completed ${completed} of ${total} tasks with ${
+                runner.concurrency
+            } concurrency`;
+        },
+        onEnd({ duration: { total } }) {
+            button.removeAttribute('disabled');
+            end.innerText = `✨ Completed all tasks in: ${(
+                total / 1000
+            ).toFixed(2)}s`;
+        }
+    });
+
     button.onclick = () => {
+        runner.concurrency = concurrency.value;
         button.setAttribute('disabled', true);
         start.innerText = '';
         done.innerText = '';
         end.innerText = '';
 
-        queueTasks(runner, 1000, {
-            onStart({ duration: { start: startDate } }) {
-                start.innerText = `Added 1000 tasks on ${new Date(startDate)}`;
-            },
-            onDone({ completed, total }) {
-                done.innerText = `✅ Completed ${completed} of ${total} tasks`;
-            },
-            onEnd({ duration: { total } }) {
-                button.removeAttribute('disabled');
-                end.innerText = `✨ Completed all tasks in: ${(
-                    total / 1000
-                ).toFixed(2)}s`;
-            }
-        });
+        queueTasks(runner, 1000);
     };
 });
