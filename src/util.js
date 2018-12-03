1bca3a5d79d43555052adace091bf96f21ec95eb
Samrith Shankar <samrith.s@media.net>
2018-12-03 13:09:21 +0530

    Added string check utils

--- src/util.js
+++ src/util.js
@@ -1,6 +1,8 @@
 export const isFunction = item => typeof item === 'function';
 export const isNumber = item => typeof item === 'number';
+export const isString = item => typeof item === 'string';
 export const isArray = item => item.constructor === Array;
+export const isEmptyString = item => isString(item) && !item;
 
 export const assignFunction = item => {
     if (isFunction(item)) {
