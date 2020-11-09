fa5cf0dd9e897b0414a7f307206e851aba3942c7
Samrith Shankar <samrith.298@gmail.com>
2020-11-09 21:53:40 +0100

    fix(universal): `TaskRunner` options now properly extend data type provided to class

--- packages/universal/src/TaskRunner.ts
+++ packages/universal/src/TaskRunner.ts
@@ -4,8 +4,8 @@ import { CoreRunner, IRunnerOptions } from '@concurrent-tasks/core';
 
 import { Strategy } from './Strategy';
 
-export class TaskRunner<T> extends CoreRunner<T> {
-    constructor(options?: Partial<IRunnerOptions>) {
+export class TaskRunner<T = any> extends CoreRunner<T, IRunnerOptions<T>> {
+    constructor(options?: Partial<IRunnerOptions<T>>) {
         super(Strategy, options);
     }
 }
