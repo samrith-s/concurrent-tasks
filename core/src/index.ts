213e01b724981aeed3ebf0cba6aadca7d4c88fae
Samrith Shankar <samrith.298@gmail.com>
2021-06-16 22:48:18 +0530

    chore: export `Strategy` from core

--- core/src/index.ts
+++ core/src/index.ts
@@ -2,8 +2,7 @@
 
 export { CoreRunner } from './internals/CoreRunner';
 export { DefaultOptions } from './internals/DefaultOptions';
-export { Strategy as DefaultStrategy } from './DefaultStrategy';
+export { Strategy } from './internals/DefaultStrategy';
 export { TaskRunner } from './TaskRunner';
-export { createStrategy } from './createStrategy';
 export * from './Utils';
 export * from './Interface';
