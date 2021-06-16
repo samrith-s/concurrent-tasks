a1fae60730bb1b2204e40874c43f2b06526dc2bb
Samrith Shankar <samrith.298@gmail.com>
2021-06-16 22:42:51 +0530

    chore: update test scripts to new structure

--- scripts/core/test-core.sh
+++ scripts/core/test-core.sh
@@ -4,5 +4,6 @@ source scripts/utils/init.sh;
 
 (
     cd core;
+    export TS_NODE_COMPILER_OPTIONS='{"module": "commonjs" }';
     $NODE_BIN_CORE/mocha -r ts-node/register './tests/**/*.ts';
 )
--- scripts/strategies/test-strategy.sh
+++ scripts/strategies/test-strategy.sh
@@ -4,5 +4,6 @@ source scripts/utils/init.sh;
 
 (
     cd strategies/strategy-$1;
+    export TS_NODE_COMPILER_OPTIONS='{"module": "commonjs" }';
     $NODE_BIN_PACKAGES/mocha -r ts-node/register './tests/**/*.ts';
 )
