704f9aeaacafa87c7f3ef7dfc8c51b3c2e895cba
Samrith Shankar <samrith.298@gmail.com>
2024-11-13 12:22:01 +0530

    chore: update release config and commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -5,7 +5,15 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", ["feat", "fix", "refactor", "deps", "docs", "test", "chore"]],
-    "scope-enum": [2, "always", ["core", "interface", "runner", "utils", "options", "logs"]],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "refactor", "deps", "docs", "test", "chore", "release"],
+    ],
+    "scope-enum": [
+      2,
+      "always",
+      ["core", "interface", "runner", "utils", "options", "logs"],
+    ],
   },
 };
